ba786dac51b4e22529a566fc518a46e0c3173499
Martin Garton <garton@gmail.com>
2022-01-25 15:33:45 +0000

    Revert "wip# Changes to be committed:"
    This reverts commit 3e75a226b7356b43c49695a25bfa78ea97c247e4.

--- src/packet.rs
+++ src/packet.rs
@@ -77,8 +77,13 @@ impl<W: Read + Write> PacketConn<W> {
 
     #[cfg(feature = "tls")]
     pub fn switch_to_tls(&mut self, config: std::sync::Arc<ServerConfig>) -> io::Result<()> {
-        self.rw
-            .switch_to_tls(config, &self.bytes[self.bytes.len() - self.remaining..])
+        if self.remaining != 0 {
+            self.rw
+                .replace(&self.bytes[self.bytes.len() - self.remaining..]);
+            self.remaining = 0;
+        }
+
+        self.rw.switch_to_tls(config)
     }
 }
 
--- src/tls.rs
+++ src/tls.rs
@@ -1,4 +1,5 @@
-use std::io::{self, Cursor};
+use std::collections::VecDeque;
+use std::io::{self, BufRead, Cursor};
 use std::io::{Read, Write};
 use std::sync::Arc;
 
@@ -16,8 +17,8 @@ pub fn create_stream<T: Read + Write + Sized>(
 pub(crate) struct SwitchableConn<T: Read + Write>(Option<EitherConn<T>>);
 
 pub(crate) enum EitherConn<T: Read + Write> {
-    Plain(T),
-    Tls(rustls::StreamOwned<ServerConnection, PrependedReader<T>>),
+    Plain(BufReader<T>),
+    Tls(rustls::StreamOwned<ServerConnection, BufReader<T>>),
 }
 
 impl<T: Read + Write> Read for SwitchableConn<T> {
@@ -47,15 +48,12 @@ impl<T: Read + Write> Write for SwitchableConn<T> {
 
 impl<T: Read + Write> SwitchableConn<T> {
     pub fn new(rw: T) -> SwitchableConn<T> {
-        SwitchableConn(Some(EitherConn::Plain(rw)))
+        SwitchableConn(Some(EitherConn::Plain(BufReader::new(rw))))
     }
 
-    pub fn switch_to_tls(&mut self, config: Arc<ServerConfig>, extra: &[u8]) -> io::Result<()> {
+    pub fn switch_to_tls(&mut self, config: Arc<ServerConfig>) -> io::Result<()> {
         let replacement = match self.0.take() {
-            Some(EitherConn::Plain(plain)) => {
-                let plain = PrependedReader::new(plain, extra);
-                Ok(EitherConn::Tls(create_stream(plain, config)?))
-            }
+            Some(EitherConn::Plain(plain)) => Ok(EitherConn::Tls(create_stream(plain, config)?)),
             Some(EitherConn::Tls(_)) => Err(io::Error::new(
                 io::ErrorKind::Other,
                 "tls variant found when plain was expected",
@@ -66,35 +64,50 @@ impl<T: Read + Write> SwitchableConn<T> {
         self.0 = Some(replacement);
         Ok(())
     }
-}
 
-pub(crate) struct PrependedReader<RW: Read + Write> {
-    prepended: Option<Cursor<Vec<u8>>>,
-    inner: RW,
-}
-
-impl<RW: Read + Write> PrependedReader<RW> {
-    fn new(rw: RW, prepended: &[u8]) -> PrependedReader<RW> {
-        PrependedReader {
-            inner: rw,
-            prepended: if prepended.is_empty() {
-                None
-            } else {
-                Some(Cursor::new(prepended.to_vec()))
-            },
+    pub(crate) fn replace(&mut self, bytes: &[u8]) {
+        match &mut self.0 {
+            Some(EitherConn::Plain(p)) => p.replace(bytes),
+            _ => panic!("can not replace raw bytes."),
         }
     }
 }
 
-impl<RW: Read + Write> Read for PrependedReader<RW> {
+pub(crate) struct BufReader<RW: Read + Write> {
+    replaced: Option<Cursor<Vec<u8>>>,
+    inner: std::io::BufReader<RW>,
+}
+
+impl<RW: Read + Write> BufReader<RW> {
+    fn new(rw: RW) -> BufReader<RW> {
+        BufReader {
+            inner: std::io::BufReader::new(rw),
+            replaced: None,
+        }
+    }
+
+    fn replace(&mut self, data: &[u8]) {
+        match &mut self.replaced {
+            Some(r) => {
+                let v = r.get_mut();
+                let mut v_new = Vec::from(data);
+                v_new.extend_from_slice(v);
+                *v = v_new;
+            }
+            None => self.replaced = Some(Cursor::new(data.to_vec())),
+        }
+    }
+}
+
+impl<RW: Read + Write> Read for BufReader<RW> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        match &mut self.prepended {
+        match &mut self.replaced {
             Some(r) => {
                 let i = r.read(buf)?;
                 if i != 0 {
                     Ok(i)
                 } else {
-                    self.prepended = None;
+                    self.replaced = None;
                     self.inner.read(buf)
                 }
             }
@@ -103,13 +116,23 @@ impl<RW: Read + Write> Read for PrependedReader<RW> {
     }
 }
 
-impl<RW: Read + Write> Write for PrependedReader<RW> {
+impl<RW: Read + Write> Write for BufReader<RW> {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        self.inner.write(buf)
+        self.inner.get_mut().write(buf)
     }
 
     fn flush(&mut self) -> io::Result<()> {
-        self.inner.flush()
+        self.inner.get_mut().flush()
+    }
+}
+
+impl<RW: Read + Write> BufRead for BufReader<RW> {
+    fn fill_buf(&mut self) -> io::Result<&[u8]> {
+        self.inner.fill_buf()
+    }
+
+    fn consume(&mut self, amt: usize) {
+        self.inner.consume(amt)
     }
 }
 
